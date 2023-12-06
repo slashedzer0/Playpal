59fa7441b1556e1836b28a24531590e819f2360d
alhajjri <alhajjriprimasaputra@gmail.com>
2023-12-06 10:15:52 +0700

    menambahkah halaman register as customer

--- app.py
+++ app.py
@@ -23,5 +23,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/regis as customer')
+def regis_customer():
+    return render_template('register as customer.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
