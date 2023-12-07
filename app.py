5ea52736768e533027f7f1716da8f73a45e62619
slashedzer0 <21104062@ittelkom-pwt.ac.id>
2023-12-07 16:25:12 +0700

    menambahkan profile.html dan update route

--- app.py
+++ app.py
@@ -19,17 +19,26 @@ client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
