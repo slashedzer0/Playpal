15000fa822f027fa676fa5b009947ebc24d82892
slashedzer0 <21104062@ittelkom-pwt.ac.id>
2023-12-15 12:29:33 +0700

    update vercel config

--- app.py
+++ app.py
@@ -623,5 +623,5 @@ def complete_order():
             return redirect(url_for('home'))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
